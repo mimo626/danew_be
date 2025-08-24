d9efe706156b40fb3743b215217a534a1354211a
kangminju <yessuk5499@gmail.com>
2025-08-24 19:10:14 +0900

    회원가입 수정

--- src/main/java/com/example/danew_spring/auth/AuthController.java
+++ src/main/java/com/example/danew_spring/auth/AuthController.java
@@ -7,15 +7,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 @Slf4j
 public class AuthController {
     @Autowired
     private AuthService authService;
 
-    @PostMapping("/api/auth/login")
+    @PostMapping("/api/auth/signup")
     public User save(@RequestBody User user) {
-        log.info(user.toString());
+        log.info("signup: {}", user);
+        user.setCreatedAt(LocalDateTime.now().toString());
         return authService.save(user);
     }
 }
