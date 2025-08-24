9b45b2b46d79eed17d3723c10f0a5d8659bc3f12
kangminju <yessuk5499@gmail.com>
2025-08-24 21:27:45 +0900

    함수 구현

--- src/main/java/com/example/danew_spring/auth/AuthRepository.java
+++ src/main/java/com/example/danew_spring/auth/AuthRepository.java
@@ -7,5 +7,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthRepository extends JpaRepository<User, Long> {
+    User findByUserId(String userId);
+
+    boolean existsByUserId(String userId);
 }
 
