77874e69251364c041ee18bb803da48c52164843
kangminju <yessuk5499@gmail.com>
2025-08-25 23:56:21 +0900

    뉴스 초기 설정

--- src/main/java/com/example/danew_spring/news/NewsController.java
+++ src/main/java/com/example/danew_spring/news/NewsController.java
@@ -1,4 +1,14 @@
 package com.example.danew_spring.news;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
 public class NewsController {
+
+    @Autowired
+    private NewsService newsService;
 }
--- src/main/java/com/example/danew_spring/news/NewsService.java
+++ src/main/java/com/example/danew_spring/news/NewsService.java
@@ -1,4 +1,11 @@
 package com.example.danew_spring.news;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class NewsService {
+
+    @Autowired
+    private NewsRepository newsRepository;
 }
