f554bb426b6bbd463fc48c2a7f9d90903568abd8
kangminju <yessuk5499@gmail.com>
2025-08-25 23:56:17 +0900

    다이어리 초기 설정

--- src/main/java/com/example/danew_spring/diary/DiaryService.java
+++ src/main/java/com/example/danew_spring/diary/DiaryService.java
@@ -1,4 +1,11 @@
 package com.example.danew_spring.diary;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DiaryService {
+
+    @Autowired
+    private DiaryRepository diaryRepository;
 }
