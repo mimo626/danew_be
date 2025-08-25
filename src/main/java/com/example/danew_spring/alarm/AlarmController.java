98b840462e9f18f050d0ec139fad6dd3bf407c0b
kangminju <yessuk5499@gmail.com>
2025-08-25 23:56:09 +0900

    알람 초기 설정

--- src/main/java/com/example/danew_spring/alarm/AlarmController.java
+++ src/main/java/com/example/danew_spring/alarm/AlarmController.java
@@ -1,4 +1,14 @@
 package com.example.danew_spring.alarm;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Slf4j
 public class AlarmController {
+
+    @Autowired
+    private AlarmService alarmService;
 }
--- src/main/java/com/example/danew_spring/alarm/AlarmService.java
+++ src/main/java/com/example/danew_spring/alarm/AlarmService.java
@@ -1,4 +1,10 @@
 package com.example.danew_spring.alarm;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AlarmService {
+    @Autowired
+    private AlarmRepository alarmRepository;
 }
