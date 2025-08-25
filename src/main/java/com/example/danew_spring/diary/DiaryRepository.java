d3a603bb6527102f334307015c550f74254877e5
kangminju <yessuk5499@gmail.com>
2025-08-25 23:55:55 +0900

    다이어리 레포지토리 생성

---
+++ src/main/java/com/example/danew_spring/diary/DiaryRepository.java
@@ -0,0 +1,9 @@
+package com.example.danew_spring.diary;
+
+import com.example.danew_spring.diary.domain.Diary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DiaryRepository extends JpaRepository<Diary, Long> {
+}
