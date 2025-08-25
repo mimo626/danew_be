ff73f88bd7f78d9d7cede84490341b9e2d689a41
kangminju <yessuk5499@gmail.com>
2025-08-25 23:50:43 +0900

    북마크 모델 구현

---
+++ src/main/java/com/example/danew_spring/news/domain/Bookmark.java
@@ -0,0 +1,32 @@
+package com.example.danew_spring.news.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@ToString
+@Slf4j
+public class Bookmark {
+
+    @Id
+    @Column(length = 45)
+    private String userId;   // PK (단일)
+
+    @Column(length = 45, nullable = false)
+    private String articleId;
+
+    private LocalDateTime bookmarkedAt;
+
+    // 생성자
+    public Bookmark(String userId, String articleId, LocalDateTime bookmarkedAt) {
+        this.userId = userId;
+        this.articleId = articleId;
+        this.bookmarkedAt = bookmarkedAt;
+    }
+}
