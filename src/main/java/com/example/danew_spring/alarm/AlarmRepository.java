32a3eeae06768b4a9391bfe3fcae6a9f183f853a
kangminju <yessuk5499@gmail.com>
2025-08-25 23:56:01 +0900

    알람 레포지토리 생성

---
+++ src/main/java/com/example/danew_spring/alarm/AlarmRepository.java
@@ -0,0 +1,9 @@
+package com.example.danew_spring.alarm;
+
+import com.example.danew_spring.alarm.domain.Alarm;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlarmRepository extends JpaRepository<Alarm, Long> {
+}
