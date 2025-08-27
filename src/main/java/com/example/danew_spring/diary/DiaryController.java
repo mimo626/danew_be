657e197179a987095d36b16003b17c728ce89118
kangminju <yessuk5499@gmail.com>
2025-08-27 23:58:11 +0900

    다이어리 컨트롤러 api 생성

--- src/main/java/com/example/danew_spring/diary/DiaryController.java
+++ src/main/java/com/example/danew_spring/diary/DiaryController.java
@@ -1,8 +1,11 @@
 package com.example.danew_spring.diary;
 
 
+import com.example.danew_spring.diary.domain.Diary;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +14,11 @@ public class DiaryController {
 
     @Autowired
     private DiaryService diaryService;
+
+    @PostMapping("api/diary/create")
+    public Diary save(@RequestBody Diary diary) {
+        log.info("save Diary: {}", diary);
+        Diary savedDiary = diaryService.save(diary);
+        return savedDiary;
+    }
 }
