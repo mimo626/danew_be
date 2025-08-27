f9e4be4ede12879a389f5e240a2c8e86969233dc
kangminju <yessuk5499@gmail.com>
2025-08-27 23:58:22 +0900

    다이어리 서비스 함수 생성

--- src/main/java/com/example/danew_spring/diary/DiaryService.java
+++ src/main/java/com/example/danew_spring/diary/DiaryService.java
@@ -1,5 +1,6 @@
 package com.example.danew_spring.diary;
 
+import com.example.danew_spring.diary.domain.Diary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +9,15 @@ public class DiaryService {
 
     @Autowired
     private DiaryRepository diaryRepository;
+
+    public Diary save(Diary diary) {
+        return diaryRepository.save(diary);
+    }
+    public Diary findByDiaryId(String diaryId) {
+        return diaryRepository.findByDiaryId(diaryId);
+    }
+
+    public void deleteByDiaryId(String diaryId) {
+        diaryRepository.deleteByDiaryId(diaryId);
+    }
 }
