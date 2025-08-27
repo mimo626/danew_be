60e6cfea6f9490ba7d612236f6b2d8169e0eb182
kangminju <yessuk5499@gmail.com>
2025-08-28 00:28:50 +0900

    다이어리 조회 함수 구현

--- src/main/java/com/example/danew_spring/diary/DiaryRepository.java
+++ src/main/java/com/example/danew_spring/diary/DiaryRepository.java
@@ -4,9 +4,15 @@ import com.example.danew_spring.diary.domain.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
     Diary findByDiaryId(String diaryId);
 
     void deleteByDiaryId(String diaryId);
+
+    Optional<Diary> findByUserIdAndCreatedAt(String userId, LocalDate createdAt);
+
 }
