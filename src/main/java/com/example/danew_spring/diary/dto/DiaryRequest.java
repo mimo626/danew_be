3056ddc0e22f0de5c1787c1ccf81de78c42713db
kangminju <yessuk5499@gmail.com>
2025-08-28 00:43:45 +0900

    DiaryRequest 구현

---
+++ src/main/java/com/example/danew_spring/diary/dto/DiaryRequest.java
@@ -0,0 +1,14 @@
+package com.example.danew_spring.diary.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DiaryRequest {
+    private String content;
+}
