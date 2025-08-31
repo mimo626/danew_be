64b345c0b3484676405fdafe52d0138bc829d21a
kangminju <yessuk5499@gmail.com>
2025-09-01 01:38:34 +0900

    findByArticleIdIn 추가

--- src/main/java/com/example/danew_spring/news/NewsRepository.java
+++ src/main/java/com/example/danew_spring/news/NewsRepository.java
@@ -4,6 +4,9 @@ import com.example.danew_spring.news.domain.News;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NewsRepository extends JpaRepository<News, String> {
+    List<News> findByArticleIdIn(List<String> articleId);
 }
