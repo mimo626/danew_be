c062bba2f259b92f6d51459ddcf2a52d21e61288
kangminju <yessuk5499@gmail.com>
2025-09-01 01:38:02 +0900

    BookmarkRepository 구현

---
+++ src/main/java/com/example/danew_spring/bookmark/BookmarkRepository.java
@@ -0,0 +1,12 @@
+package com.example.danew_spring.bookmark;
+
+import com.example.danew_spring.news.domain.Bookmark;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+    List<Bookmark> findByUserId(String userId);
+}
