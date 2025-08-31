54714ee50641c4727b14ef6d05c3960ae32578f5
kangminju <yessuk5499@gmail.com>
2025-09-01 03:49:23 +0900

    News 필드 타입 수정

--- src/main/java/com/example/danew_spring/news/domain/News.java
+++ src/main/java/com/example/danew_spring/news/domain/News.java
@@ -18,12 +18,12 @@ public class News {
 
     @Id
     @Column(length = 45)
-    private String articleId;   // PK
+    private String id;   // PK
 
     @Column(length = 255)
     private String title;
 
-    @Column(length = 255)
+    @Column(columnDefinition = "TEXT(1000)")
     private String link;
 
     @Convert(converter = StringListConverter.class)
@@ -32,13 +32,13 @@ public class News {
     @Convert(converter = StringListConverter.class)
     private List<String> creator;
 
-    @Column(length = 45)
+    @Column(columnDefinition = "TEXT(1000)")
     private String description;
 
     @Column(length = 45)
     private String pubDate;
 
-    @Column(length = 255)
+    @Column(columnDefinition = "TEXT(1000)")
     private String imageUrl;
 
     @Column(length = 45)
@@ -52,11 +52,11 @@ public class News {
 
     private LocalDate savedAt;
 
-    public News(String articleId, String title, String link,
+    public News(String id, String title, String link,
                 List<String> keywords, List<String> creator, String description,
                 String pubDate, String imageUrl, String sourceName,
                 String language, List<String> category, LocalDate savedAt) {
-        this.articleId = articleId;
+        this.id = id;
         this.title = title;
         this.link = link;
         this.keywords = keywords;
