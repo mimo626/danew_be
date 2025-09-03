564586d4fe0a17b3b614e34b15f1f0090cbca984
kangminju <yessuk5499@gmail.com>
2025-09-03 15:42:56 +0900

    롬복 어노테이션 추가

--- src/main/java/com/example/danew_spring/ApiResponse.java
+++ src/main/java/com/example/danew_spring/ApiResponse.java
@@ -1,15 +1,18 @@
 package com.example.danew_spring;
 
-public class ApiResponse<T> {
-    private String status; // "success" or "error"
-    private String message; // 필요시 오류 메시지
-    private T data; // 실제 반환할 데이터
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-    // 생성자
+@Data
+@NoArgsConstructor
+public class ApiResponse<T> {
+    private String status;
+    private String message;
+    private T data;
+
     public ApiResponse(String status, String message, T data) {
         this.status = status;
         this.message = message;
         this.data = data;
     }
-    // getters/setters 생략
 }
