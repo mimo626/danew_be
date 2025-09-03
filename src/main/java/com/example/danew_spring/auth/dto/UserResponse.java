94ee71ca81fa05d70b68f6411c1b4c839874c3dd
kangminju <yessuk5499@gmail.com>
2025-09-03 15:42:58 +0900

    롬복 어노테이션 추가

--- src/main/java/com/example/danew_spring/auth/dto/UserResponse.java
+++ src/main/java/com/example/danew_spring/auth/dto/UserResponse.java
@@ -1,13 +1,19 @@
 package com.example.danew_spring.auth.dto;
 
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class UserResponse {
     private String userId;
+    private String token;
     private String createdAt;
 
-    public UserResponse(String userId, String createdAt) {
+    public UserResponse(String userId, String token, String createdAt) {
         this.userId = userId;
+        this.token = token;
         this.createdAt = createdAt;
     }
-
-    // getters/setters 생략
 }
