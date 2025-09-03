9d9c3591d7a29f595ae418a09d117ba3ff6b775f
kangminju <yessuk5499@gmail.com>
2025-09-03 20:19:23 +0900

    북마크 api 반환값 수정

--- src/main/java/com/example/danew_spring/bookmark/BookmarkController.java
+++ src/main/java/com/example/danew_spring/bookmark/BookmarkController.java
@@ -1,11 +1,14 @@
 package com.example.danew_spring.bookmark;
 
+import com.example.danew_spring.ApiResponse;
 import com.example.danew_spring.JwtTokenProvider;
 import com.example.danew_spring.news.NewsService;
 import com.example.danew_spring.news.domain.Bookmark;
 import com.example.danew_spring.news.domain.News;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,9 +29,9 @@ public class BookmarkController {
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
 
-    @PostMapping("/save")
-    public ResponseEntity<Bookmark> saveBookmark(@RequestBody News newsRequest,
-                                                 @RequestHeader("Authorization") String token) {
+    @PostMapping(value = "/save", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ApiResponse<Bookmark>> saveBookmark(@RequestBody News newsRequest,
+                                                              @RequestHeader("Authorization") String token) {
         // 1) 토큰에서 userId 추출
         String userId = jwtTokenProvider.getUserIdFromToken(token.replace("Bearer ", ""));
 
@@ -41,34 +44,47 @@ public class BookmarkController {
         Bookmark bookmark = new Bookmark(userId, newsRequest.getId(), LocalDate.now());
         Bookmark savedBookmark = bookmarkService.save(bookmark);
 
-        log.info("save bookmark: {}", savedBookmark);
+        log.info("북마크 저장: {}", savedBookmark);
 
-        return ResponseEntity.ok(savedBookmark);
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Boolean> deleteBookmark(@PathVariable String id,
-                                                  @RequestHeader("Authorization") String token) {
-        try {
-            String userId = jwtTokenProvider.getUserIdFromToken(token.replace("Bearer ", ""));
-            bookmarkService.deleteByUserIdAndArticleId(userId, id);
-            log.info("delete bookmark: {}", id);
-
-            return ResponseEntity.ok(true);
-        } catch (Exception e) {
-            log.error("북마크 삭제 실패: {}", id, e);
-            return ResponseEntity.ok(false);
+        if(savedBookmark == null) {
+            ResponseEntity.status(HttpStatus.OK)
+                    .body(new ApiResponse<>("error", "북마크 저장 실패", null));
         }
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponse<>("success", "북마크 저장 성공", savedBookmark));
     }
 
-
-    @GetMapping("/getBookmarks")
-    public ResponseEntity<List<News>> getBookmarks(
+    @GetMapping(value = "/getBookmarks", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ApiResponse<List<News>>> getBookmarks(
             @RequestHeader("Authorization") String token) {
 
         String userId = jwtTokenProvider.getUserIdFromToken(token.replace("Bearer ", ""));
 
         List<News> bookmarks = bookmarkService.getUserBookmarks(userId);
-        return ResponseEntity.ok(bookmarks);
+        log.info("북마크 뉴스 조회: {}", bookmarks);
+
+        if(bookmarks.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>("error", "북마크 뉴스 조회 실패", null));
+        }
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponse<>("success", "북마크 뉴스 조회 성공", bookmarks));    }
+
+    @DeleteMapping(value = "/delete/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ApiResponse<Boolean>> deleteBookmark(@PathVariable String id,
+                                                  @RequestHeader("Authorization") String token) {
+        try {
+            String userId = jwtTokenProvider.getUserIdFromToken(token.replace("Bearer ", ""));
+            bookmarkService.deleteByUserIdAndArticleId(userId, id);
+            log.info("북마크 삭제: {}", id);
+
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new ApiResponse<>("success", "북마크 삭제 성공", true));
+        } catch (Exception e) {
+            log.error("북마크 삭제 실패: {}", id, e);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new ApiResponse<>("error", "북마크 삭제 실패", false));
+        }
     }
 }
