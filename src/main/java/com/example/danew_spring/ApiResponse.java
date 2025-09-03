139ef2aae921a5fb11b9b8568d42b8cea24bc086
kangminju <yessuk5499@gmail.com>
2025-09-03 15:44:56 +0900

    주석 추가

--- src/main/java/com/example/danew_spring/ApiResponse.java
+++ src/main/java/com/example/danew_spring/ApiResponse.java
@@ -3,8 +3,10 @@ package com.example.danew_spring;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
+@Data //getter/setter 자동 생성
+@NoArgsConstructor //Jackson 기본 생성자
+//위 두개가 없으면 JSON 직렬화 불가
+
 public class ApiResponse<T> {
     private String status;
     private String message;
