a01f7f49f475f3d9bc95feac2e3f044a7a7dcd05
kangminju <yessuk5499@gmail.com>
2025-10-14 03:12:41 +0900

    테스트 파일 수정

--- src/test/java/com/example/danew_spring/DanewSpringApplicationTests.java
+++ src/test/java/com/example/danew_spring/DanewSpringApplicationTests.java
@@ -15,19 +15,19 @@ class DanewSpringApplicationTests {
 	@Autowired
 	private AuthService authService;
 
-	@Test
-	public void addUserTest() {
-		User user = new User();
-		user.setUserId("0626");
-		user.setPassword("123456");
-		user.setName("강민주");
-		user.setAge(24);
-		user.setGender("여성");
-		user.setCreatedAt(LocalDateTime.now().toString());
-		user.setKeywordList(List.of(new String[]{"정치", "IT", "AI"}));
-		user.setCustomList(List.of(new String[]{}));
-
-		authService.save(user);
-	}
+//	@Test
+//	public void addUserTest() {
+//		User user = new User();
+//		user.setUserId("0626");
+//		user.setPassword("123456");
+//		user.setName("강민주");
+//		user.setAge(24);
+//		user.setGender("여성");
+//		user.setCreatedAt(LocalDateTime.now().toString());
+//		user.setKeywordList(List.of(new String[]{"정치", "IT", "AI"}));
+//		user.setCustomList(List.of(new String[]{}));
+//
+//		authService.save(user);
+//	}
 
 }
