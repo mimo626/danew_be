e963881e073e51f48b45f51a85d39262c0c9e553
kangminju <yessuk5499@gmail.com>
2025-11-24 00:24:55 +0900

    FcmConfig 구현

---
+++ src/main/java/com/example/danew_spring/firebase/FcmConfig.java
@@ -0,0 +1,32 @@
+package com.example.danew_spring.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+@Slf4j
+@Configuration
+public class FcmConfig {
+    @PostConstruct
+    public void initialize() {
+        try {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(
+                            GoogleCredentials.fromStream(new ClassPathResource("access_key.json").getInputStream())
+                    )
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+            log.info("Fcm 설정 성공");
+
+        } catch (IOException exception) {
+            log.error("Fcm 연결 오류 {}", exception.getMessage());
+        }
+    }
+}
