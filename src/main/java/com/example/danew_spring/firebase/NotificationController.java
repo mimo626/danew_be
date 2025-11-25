0eb962fc9b77b210f3f459726bff237bf1b1d5e4
kangminju <yessuk5499@gmail.com>
2025-11-25 22:57:38 +0900

    NotificationController 수정

--- src/main/java/com/example/danew_spring/firebase/NotificationController.java
+++ src/main/java/com/example/danew_spring/firebase/NotificationController.java
@@ -14,9 +14,9 @@ public class NotificationController {
     @PostMapping("/send")
     public ResponseEntity<String> sendNotification(
             @RequestHeader("X-FCM-Token") String token, // 헤더로 토큰 받기
-            @RequestParam String nickname
+            @RequestParam String title, @RequestParam String content
     ) {
-        notificationService.sendEntryNotification(token, nickname);
+        notificationService.sendEntryNotification(token, title, content);
         return ResponseEntity.ok("알림 전송 요청 완료");
     }
 }
